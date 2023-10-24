16223934d04bb9c110a0b48e578a4cfca113e4be
nourelhoudaas <sayahhouda06@gmail.com>
2023-10-24 21:54:47 +0100

    Edit index.html and script.js

--- script.js
+++ script.js
@@ -1,72 +1,77 @@
+       
+let playerScore = 0;
+let computerScore = 0;
+
+// Fonction pour obtenir le choix aléatoire de l'ordinateur
 function getComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * 3);
-    let choix;
-
-    if (randomNumber === 0) {
-        choix = "rock";
-    } else if (randomNumber === 1) {
-        choix = "paper";
-    } else {
-        choix = "scissors";
-    }
-
-    return choix;
+    const choices = ["Rock", "Paper", "Scissors"];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex];
 }
 
-function playRound(playerSelection) {
-    const computerSelection = getComputerChoice();
-    playerSelection = playerSelection.toLowerCase();
+// Fonction pour jouer un tour de pierre-papier-ciseaux
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase(); // Convertir en minuscules pour être insensible à la casse
 
-
-    // Vérifier si l'entrée de l'utilisateur est valide
-    if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
-        return "It's an invalid selection. Please choose Rock, Paper, or Scissors.";
-    }
-
-    if (computerSelection === "rock" && playerSelection === "scissors") {
-        return "You Lose! Rock beats Scissors";
-    } else if (computerSelection === "paper" && playerSelection === "scissors") {
-        return "You Win! Scissors beats Paper";
-    } else if (computerSelection === "rock" && playerSelection === "paper") {
-        return "You Win! Paper beats Rock";
-    } else if (computerSelection === playerSelection) {
-        return "It's a tie!";
-    } else {
-        return "It's an invalid selection. Please choose Rock, Paper, or Scissors.";
-    }
-}
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-  
-    for (let i = 0; i < 5; i++) {
-      const playerSelection = prompt("Rock, paper, or scissors?");
-      const computerSelection = getComputerChoice();
-      console.log(playRound(playerSelection, computerSelection));
-      if (playRound(playerSelection, computerSelection) === "You Win!") {
+    if (playerSelection === computerSelection) {
+        return "It's a tie! " + playerSelection + " equals " + computerSelection;
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
         playerScore++;
-      } else if (playRound(playerSelection, computerSelection) === "You Lose!") {
-        computerScore++;
-      }
-    }
-  
-    if (playerScore > computerScore) {
-      console.log("You win the game!");
-    } else if (playerScore < computerScore) {
-      console.log("You lose the game!");
+        return "You Win! " + playerSelection + " beats " + computerSelection;
     } else {
-      console.log("The game is a tie!");
+        computerScore++;
+        return "You Lose! " + computerSelection + " beats " + playerSelection;
     }
-  }
-  
-  game();
+}
 
-/*const playerSelection = prompt("Choisissez : Rock, Paper ou Scissors").trim();
+// Fonction pour mettre à jour l'affichage du score
+function updateScore() {
+    document.getElementById('score').textContent = "Score: Player " + playerScore + " - Computer " + computerScore;
+}
 
-const result = playRound(playerSelection);
-console.log("Le résultat : " + result);
+// Écouteurs d'événements pour les boutons de choix du joueur
+document.getElementById('rock').addEventListener('click', () => {
+    const computerChoice = getComputerChoice();
+    const result = playRound("rock", computerChoice);
+    document.getElementById('result').textContent = result;
+    updateScore();
+    checkWinner();
+});
 
-// L'appel à getComputerChoice pour obtenir le choix de l'ordinateur
-const computerSelection = getComputerChoice();
-console.log("Choix de l'ordinateur : " + computerSelection);
-*/
+document.getElementById('paper').addEventListener('click', () => {
+    const computerChoice = getComputerChoice();
+    const result = playRound("paper", computerChoice);
+    document.getElementById('result').textContent = result;
+    updateScore();
+    checkWinner();
+});
+
+document.getElementById('scissors').addEventListener('click', () => {
+    const computerChoice = getComputerChoice();
+    const result = playRound("scissors", computerChoice);
+    document.getElementById('result').textContent = result;
+    updateScore();
+    checkWinner();
+});
+
+// Fonction pour vérifier le gagnant du jeu
+function checkWinner() {
+    if (playerScore === 5) {
+        document.getElementById('result').textContent = "Player wins the game!";
+        disableButtons();
+    } else if (computerScore === 5) {
+        document.getElementById('result').textContent = "Computer wins the game!";
+        disableButtons();
+    }
+}
+
+// Fonction pour désactiver les boutons une fois le jeu terminé
+function disableButtons() {
+    document.getElementById('rock').disabled = true;
+    document.getElementById('paper').disabled = true;
+    document.getElementById('scissors').disabled = true;
+}
